439abe3b4165328c4e7ca581c4c17aed1af6e8b4
Jonathan Craddock <jonathancraddock@users.noreply.github.com>
2021-06-05 01:52:07 +0100

    Comment added, "Execute" permission required

--- inject.sh
+++ inject.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Requires "Execute" permission
+# chmod +x inject.sh
+
 # Timeout after 1 second
 # Return only final line of status ( null | Terminated )
 timeout 1s P4wnP1_cli hid run -c 'press("SHIFT");delay(50);type('\"$1\"')' -t 1 | tail -n 1
