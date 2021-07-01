6b918ca88bee792200ea7c32d502407029a205c5
Jonathan Craddock <jonathancraddock@users.noreply.github.com>
2021-07-01 18:23:08 +0100

    Specify GB keyboard layout

--- inject.sh
+++ inject.sh
@@ -3,6 +3,8 @@
 # Requires "Execute" permission
 # chmod +x inject.sh
 
+# 1/7/21, Added 'layout("gb"); to beginning of command
+
 # Timeout after 1 second
 # Return only final line of status ( null | Terminated )
-timeout 1s P4wnP1_cli hid run -c 'press("SHIFT");delay(50);type('\"$1\"')' -t 1 | tail -n 1
+timeout 1s P4wnP1_cli hid run -c 'layout("gb");press("SHIFT");delay(50);type('\"$1\"')' -t 1 | tail -n 1
