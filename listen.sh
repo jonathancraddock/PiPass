53ee15f14fad3a802beb384a385ddbe99480444b
Jonathan Craddock <jonathancraddock@users.noreply.github.com>
2021-07-02 13:16:52 +0100

    Bash script to return 'ok' status

---
+++ listen.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Requires "Execute" permission
+# chmod +x inject.sh
+
+# 2/7/21, heartbeat functionality - is PiPass listening?
+
+# jQuery call wil timeout after 250ms
+# Return 'ok'
+echo ok
